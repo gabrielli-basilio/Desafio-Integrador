debb6fc86daa5cfffc4aa8b3fa30970a152f1aed
lucas <engs-lucassantos2@camporeal.edu.br>
2026-05-31 19:33:32 -0300

    feat: adiciona enums Categoria e StatusPedido

--- src/com/gestaopedidos/infra/ConexaoBanco.java
+++ src/com/gestaopedidos/infra/ConexaoBanco.java
@@ -1,4 +1,4 @@
-package com.gestãoPedidos.infra;
+package com.gestaopedidos.infra;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
---
+++ src/com/gestaopedidos/model/enums/StatusPedido.java
@@ -0,0 +1,8 @@
+package com.gestaopedidos.model.enums;
+
+public enum StatusPedido {
+    ABERTO,
+    FILA,
+    PROCESSANDO,
+    FINALIZADO
+}
