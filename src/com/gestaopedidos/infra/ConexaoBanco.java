cf9b7369aacae626580a2cdff7398bf18b61aec4
Gabi <engs-gabrielliengs-gabrielliferreira@camporeal.edu.br>
2026-06-06 11:44:52 -0300

    Ajuste na conexão banco para acesso em nuvem

--- src/com/gestaopedidos/infra/ConexaoBanco.java
+++ src/com/gestaopedidos/infra/ConexaoBanco.java
@@ -7,9 +7,9 @@ import java.sql.SQLException;
 public class ConexaoBanco {
 
     private static final String URL =
-        "jdbc:mysql://localhost:3306/gerenciamento_pedidos?useUnicode=true&characterEncoding=UTF-8";
+        "jdbc:mysql://monorail.proxy.rlwy.net:36115/railway?useUnicode=true&characterEncoding=UTF-8";
     private static final String USER = "root";
-    private static final String PASS = "";
+    private static final String PASS = "456123";
 
     public static Connection conectar() throws SQLException {
         return DriverManager.getConnection(URL, USER, PASS);
