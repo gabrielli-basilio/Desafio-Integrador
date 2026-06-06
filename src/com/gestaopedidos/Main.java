3570eb5bd3bec6a67b17cd841528aa9bf4fc1890
lucas <engs-lucassantos2@camporeal.edu.br>
2026-06-06 09:30:59 -0300

    feat: adição do main

---
+++ src/com/gestaopedidos/Main.java
@@ -0,0 +1,22 @@
+package com.gestaopedidos;
+
+import com.gestaopedidos.thread.ProcessadorPedidos;
+import com.gestaopedidos.ui.MenuPrincipal;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        ProcessadorPedidos processador = new ProcessadorPedidos();
+        Thread thread = new Thread(processador);
+        thread.setDaemon(true);
+        thread.start();
+
+        Scanner scanner = new Scanner(System.in);
+        MenuPrincipal menu = new MenuPrincipal(scanner);
+        menu.exibir();
+
+        scanner.close();
+    }
+}
